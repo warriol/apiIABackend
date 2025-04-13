c4183c90b0baf97fbd4c9f649687cb751cca6e8e
wilson arriola <warriol@gmail.com>
2025-04-13 19:59:10 -0300

    limpiando del repo archvos de prueba

--- server.py
+++ server.py
@@ -1,7 +1,5 @@
-# ngrok http --url=deciding-rested-badger.ngrok-free.app 127.0.0.1:5000
-
-import os
 import requests
+import pandas as pd
 from flask import Flask, request, jsonify, Response
 from flask_cors import CORS
 
@@ -9,19 +7,8 @@ app = Flask(__name__)
 CORS(app)
 
 OLLAMA_API_URL = "http://localhost:11434/api/generate"
-MODEL_NAME = "gemma3:4b"
 
-def download_model(model_name):
-    response = requests.post(f"{OLLAMA_API_URL}/pull", json={"model": model_name})
-    if response.status_code == 200:
-        print(f"Modelo {model_name} descargado exitosamente.")
-    else:
-        print(f"Error al descargar el modelo {model_name}: {response.text}")
-
-@app.before_request
-def setup():
-    download_model(MODEL_NAME)
-
+train_data = pd.read_csv("train.csv", sep=",", encoding="utf-8")
 @app.route('/chat', methods=['POST'])
 def chat():
     data = request.json
@@ -33,26 +20,42 @@ def chat():
 
     print(f"Llegó una pregunta: {question}")
 
-    promptL = (
-        "Eres un abogado experto en derecho penal uruguayo. "
-        "Responde siempre en español, utilizando un lenguaje técnico pero claro. "
-        "Debes fundamentar tus respuestas con base en el Código Penal y Código de Faltas de Uruguay. "
-        "Si la pregunta no está relacionada con derecho penal, indica que no puedes responder. "
-        f"Pregunta del usuario: {question}"
-    )
+    if role == "leyes":
+        prompt = (
+            "Eres un abogado experto en derecho penal uruguayo. "
+            "Responde siempre en español, utilizando un lenguaje técnico pero claro. "
+            "Debes fundamentar tus respuestas con base en el Código Penal y Código de Faltas de Uruguay. "
+            "Si la pregunta no está relacionada con derecho penal, indica que no puedes responder. "
+            f"Pregunta del usuario: {question}"
+        )
+    elif role == "ortografia":
+        prompt = f"""Eres un profesor de lengua española con excelente ortografía y gramática, tienes como tareas:
+            - Revisarás el texto que se te envíe y harás las correcciones necesarias.
+            - Verifica que al comienzo de cada párrafo haya una sangría de 5 espacios.
+            - Entre un párrafo y el siguiente, debe haber un interlineado.
+            - Si hay texto entre comillas indica que es un pasaje textual, se espera que no hagas correcciones orográficas o gramaticales de los pasajes textuales, pero si el mismo esta escrito en minúsculas debes cambiar el texto del pasaje textual a mayúsculas.
+            - Presentarás como respuesta el texto corregido y al final una lista con las correcciones que hayas realizado.
+            Texto del usuario: {question}"""
+    elif role == "sgsp":
+        context = "\n".join(
+            f"{row['Contexto']}: {row['Response']}" for  _, row in train_data.iterrows()
+        )
+        prompt = (
+            "Eres un experto en el Sistema de Gestión de Seguridad Pública (SGSP). "
+            "Responde siempre en español, utilizando un lenguaje técnico pero claro. "
+            "Si recibes un soludo como pregunta, responde con un saludo y un resumen de tus capaciodades. "
+            "Utiliza el siguiente contexto para responder las preguntas del usuario:\n"
+            f"{context}\n"
+            f"Pregunta del usuario: {question}"
+        )
 
-    promptO = (
-        "Eres un profesor de lengua española con excelente ortografía y gramática. "
-        "Revisarás el texto que se te envíe y harás las correcciones necesarias. "
-        "Presentarás como respuesta el texto corregido y al final una lista de faltas encontradas y su corrección. "
-        f"Texto del usuario: {question}"
-    )
+    else:
+        return jsonify({"response": "Rol no reconocido."}), 400
 
-    prompt = promptL if role == "leyes" else promptO
-
+    # Enviar pregunta a la API de Ollama
     try:
         payload = {
-            "model": MODEL_NAME,
+            "model": "gemma3:4b",
             "prompt": prompt,
             "stream": True,
             "max_tokens": 512
